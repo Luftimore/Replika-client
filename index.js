1c622dc67bf9a92269cab69aa1ca5c9b2c645f64
lufti <lufti@localhost.localdomain>
2023-07-15 19:33:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('https://my.replika.ai');
+}
+
+app.whenReady().then(createWindow);
